a73ecc58fa96ceea0407dece5447f7b7b59e7e85
rithanya141516 <rithanya141516@gmail.com>
2025-12-26 19:29:33 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
+from data_loader import search_products
 
 app = FastAPI()
 
